551213ae86da11f4bc4f11807d81532e4f398373
ldv2018 <ldv2018@yandex.ru>
2024-04-03 19:17:05 +0300

    create project

---
+++ src/main/java/com/example/treebuilder/TreeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.treebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/treebuilder/TreeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
